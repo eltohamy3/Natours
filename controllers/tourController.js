f5870d4e5f29a010d69a95289a7728cb146b5492
Boda-Eltohamy <aboudeltohamy@gmail.com>
2025-01-29 22:20:35 +0200

    Adding the aggregation Pipline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -105,3 +105,91 @@ exports.aliasTopTours = (req, res, next) => {
   req.query.fields = "name,price,ratingsAverage,summary,difficulty";
   next();
 };
+exports.getTourStats = async (req, res) => {
+  // stats about tours
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: {
+          ratingsAverage: { $gte: 4.5 },
+        },
+      },
+      {
+        $group: {
+          _id: null,
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+          numTours: { $sum: 1 },
+          numOfRatings : { $sum : '$ratingsQuantity' },
+        },
+      },
+    ]);
+    res.status(200).json({
+      status: "success",
+      data: {
+        stats: stats,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failure",
+      message: err,
+    });
+  }
+};
+exports.getMonthlyPlan = async (req, res) =>{
+   try{
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+
+      {
+        $unwind: "$startDates" 
+      } ,
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numTours: { $sum: 1 },
+          tours: {$push : '$name'}  
+        },
+      } ,
+      {
+        $addFields : { month : "$_id"}
+      },
+      {
+        $project : { _id: 0 }
+      } , 
+      {
+        $sort: { _id: 1 },
+      },
+
+
+    ]);
+    res.status(200).json({
+      
+      status: "success",
+      lenght: plan.length,
+      data: {
+        plan: plan,
+      },
+
+    });
+   }catch(err){
+
+    res.status(400).json({
+      status: "failure",
+      message: err,
+    });
+   };
+   
+
+}
