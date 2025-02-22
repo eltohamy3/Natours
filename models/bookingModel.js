32f11a4a057888d5a8265ad4fc64d04916ad0b0b
Boda-Eltohamy <aboudeltohamy@gmail.com>
2025-02-22 11:49:21 +0200

    Adding the booking Model

---
+++ models/bookingModel.js
@@ -0,0 +1,40 @@
+const mongoose = require ('mongoose'); 
+
+const bookingSchema = mongoose.Schema({
+  user : {
+    type : mongoose.Schema.ObjectId ,
+    ref : 'User' ,
+    required :[true , 'Booking must belong to a Tour!.']
+  }  , 
+  tour:{
+    type : mongoose.Schema.ObjectId ,
+    ref : 'Tour' ,
+    required :[true , 'Booking must belong to a user!.']
+  } ,
+  price : {
+    type : Number ,
+    require :[true , 'Bokking must have a price']
+  },
+  cretedAt :{
+    type : Date , 
+    default : Date.now()
+  } ,
+  paid :{ // if the admin make a booking outside a stripe 
+    type: Boolean ,
+    default : true
+  }
+  
+});
+
+bookingSchema.pre(/^find/ , function (next) {
+  this.populate('user').populate('user').populate({
+    path: 'tour' ,
+    select: 'name'
+  });
+
+})
+
+
+const Booking = mongoose.model('Booking' , bookingSchema); 
+
+module.exports = Booking;
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 // const slugify = require("slugify");
 const Tour = require("./tourModel");
+
 const reviewSchema = mongoose.Schema(
   {
     user: {
